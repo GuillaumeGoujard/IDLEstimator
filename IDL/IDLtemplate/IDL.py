Fixed Projection and gradients

IDL/IDLtemplate/IDL.py
@@ -54,11 +54,12 @@ class IDLEstimator(BaseEstimator):
         X = None
         lambda_dual = np.ones((self.h))
         Lambda = np.diag(lambda_dual)
-        theta = [A, B, c, D, E, f, Lambda]
+        theta = {"A": A, "B": B, "c": c, "D": D, "E": E, "f": f, "Lambda": Lambda}
 
         for k in range(100):
             L = me.Loss(y, X, theta, U)
-            theta = theta - gd.alpha_theta(X, U)@gd.gradient_descent_theta(X, theta, U, y)
+            theta = gd.update_theta(theta, X, U, Y)
+            #theta = theta - gd.alpha_theta(X, U)@gd.gradient_descent_theta(X, theta, U, y)
             theta = cp.project_to_S_theta(theta)
             X = np.maximum(0, X - gd.alpha_x(theta)@gd.gradient_descent_x(X, theta, U, y))
             lambda_dual = da.update_dual(theta, X, U)

IDL/IDLtemplate/tests/gradient_tests.py
@@ -7,32 +7,35 @@ import sys
 
 sys.path.append("../")
 
-from utilities import GradientDescent as grad
+from utilities import GradientDescents as grad
 
 
 def initial_test():
     h = 5
     p = 4
+    n = 7
     A = np.random.rand(p, h)
     D = np.random.rand(h, h)
     c = np.random.rand(p, 1)
-    B = np.random.rand(p, h)
-    E = np.random.rand(h, h)
+    B = np.random.rand(p, n)
+    E = np.random.rand(h, n)
     f = np.random.rand(h, 1)
-
-
     Lambda = np.diag(np.random.rand(h))
-    theta = {"A": A, "D": D, "Lambda": Lambda, "c": c, "B": B, "E": E, "f": f}
 
     m = 6
-    X = np.random.rand()
+
+    theta = {"A": A, "D": D, "Lambda": Lambda, "c": c, "B": B, "E": E, "f": f, "m": m}
+
+
+    X = np.random.rand(h, m)
+
+    U = np.random.rand(n, m)
+    grad_X = grad.gradient_descent_x(theta, X, U)
 
     grad_theta = grad.project_to_S_theta(theta)
-    print(result)
     a = 2
 
 
 
 if __name__ == '__main__':
-    print(dir(conv))
     initial_test()

IDL/IDLtemplate/utilities/ConvexProjection.py
@@ -11,9 +11,12 @@ def project_to_S_theta(theta, epsilon=.05):
     """
 
     Lambda = theta["Lambda"]
-    proj_A = cp.Variable(shape=theta["A"].shape, value=theta["A"])
-    proj_D = cp.Variable(shape=theta["D"].shape, value=theta["D"])
+    #proj_A = cp.Variable(shape=theta["A"].shape)#, value=theta["A"])
+    proj_D = cp.Variable(shape=theta["D"].shape)#, value=theta["D"])
+    #smth = cp.bmat([[proj_A], [proj_D]])
     h, h = theta["D"].shape
+
+    D = theta["D"]
 
     #
     #big_D = cp.Variable(value=np.block([[(1 - epsilon) * np.eye(h), theta["D"]],
@@ -29,24 +32,21 @@ def project_to_S_theta(theta, epsilon=.05):
     # Since Lambda is diagonal, the matrix multiplication below could be more efficient right?
     # -> could just sum last two terms and multiply by lambda from either the right or left
 
-    expression = Lambda - (Lambda @ proj_D + proj_D.T @ Lambda)  # + (proj_A.T) @ (proj_A)
-
-    symm_mtrx = cp.Variable(shape=theta["Lambda"].shape, PSD=True)
-
-    # We now reformulate the norm constraint on D to a LMI through Schur Complement
+    expression = Lambda - (Lambda @ proj_D + proj_D.T @ Lambda)
 
     tmp = cp.bmat([[np.diag(np.repeat(1-epsilon, h)), proj_D], [proj_D.T, np.diag(np.repeat(1-epsilon, h))]])
 
-    objective = cp.norm(symm_mtrx - expression, p="fro")
+    constraint = [expression >= 0] + [tmp >= 0]
 
-    problem = cp.Problem(cp.Minimize(objective), constraints=[tmp >= 0])
+    objective = cp.norm(D - proj_D, p="fro")
+
+    #symm_mtrx = cp.Variable(shape=theta["Lambda"].shape, PSD=True)
+    # We now reformulate the norm constraint on D to a LMI through Schur Complement
+
+    problem = cp.Problem(cp.Minimize(objective), constraints=constraint)
 
     problem.solve()
-
-    # etteren, etterto = proj_D.value, proj_A.value
-
     theta["D"] = proj_D.value
-    theta["A"] = proj_A.value
 
     return theta
 

IDL/IDLtemplate/utilities/GradientDescents.py
@@ -1,8 +1,16 @@
 import numpy as np
 
-def alpha_theta(X_, U):
-    #TODO
-    pass
+def update_theta(theta, X, U, Y):
+    grad_theta, lip_ABc, lip_DEf = gradient_descent_theta(theta, X, U, Y)
+
+
+    for key, value in grad_theta:
+        if key in ["A", "B", "c"]:
+            theta[key] -= (1 / lip_ABc) * grad_theta[key]
+        elif key in ["D", "E", "f"]:
+            theta[key] -= (1/lip_DEf) * grad_theta[key]
+
+    return theta
 
 def gradient_descent_theta(theta, X, U, Y):
     """
@@ -15,14 +23,15 @@ def gradient_descent_theta(theta, X, U, Y):
     """
     grad_theta = {}
 
-    m = np.shape(theta["c"])
+    m = theta["m"]
     AB_Omega = Omega_ABc(theta, X=X, U=U, Y=Y)
     grad_theta["A"] = AB_Omega @ X.T
     grad_theta["B"] = AB_Omega @ U.T
-    grad_theta["c"] = AB_Omega @ np.ones(m)
-    xnorm = np.linalg.norm(X, ord="fro")
-    unorm = np.linalg.norm(U, ord="fro")
-    lip_theta_one = (1/m) * max(m, xnorm ** 2, unorm ** 2, np.linalg.norm(X @ U.T, ord="fro"))
+    grad_theta["c"] = AB_Omega @ np.ones(m).reshape(m, 1)
+
+    xnorm = np.linalg.eigvals(X, ord=2)
+    unorm = np.linalg.norm(U, ord=2)
+    lip_theta_one = (1 / m) * max(m, xnorm ** 2, unorm ** 2, np.linalg.norm(X @ U.T, ord=2))
 
     DEF_Omega = Omega_DEfLambda(m, D=theta["D"], E=theta["E"], F=theta["F"],
                                 Lambda=theta["Lambda"], X=X, U=theta["U"])
@@ -33,14 +42,21 @@ def gradient_descent_theta(theta, X, U, Y):
 
     lip_theta_two = (np.max(theta["Lambda"]) / m) * max(m, xnorm ** 2, unorm ** 2, xnorm, unorm)
 
-    return grad_theta, (lip_theta_one, lip_theta_two)
+    return grad_theta, lip_theta_one, lip_theta_two
 
 def alpha_x(theta):
-    #TODO
-    pass
+    m = theta["m"]
+    A = theta["A"]
+    D = theta["D"]
+    Lambda = theta["Lambda"]
 
-def gradient_descent_x(theta, X, U, y):
-    m = np.shape(theta["c"])
+    lip_X = (1 / m) * (np.linalg.norm(A.T @ A + Lambda - Lambda @ D + D.T @ Lambda, ord="fro") +
+                       np.max(Lambda) * (np.linalg.norm(D, ord="fro") ** 2))
+
+    return 1 / lip_X
+
+def gradient_descent_x(theta, X, U):
+    m = theta["m"]
     A = theta["A"]
     B = theta["B"]
     D = theta["D"]
@@ -49,20 +65,18 @@ def gradient_descent_x(theta, X, U, y):
     c = theta["c"]
     Lambda = theta["Lambda"]
     # Make sure that the shapes from the outer products c @ np.ones(m) etc. are right
-    grad_X = (1 / m) * (A.T @ (A @ X + B @ U + c @ np.ones(m).T) +
+    grad_X = (1 / m) * (A.T @ (A @ X + B @ U + c @ np.ones(m).reshape(1, m)) +
                                  (Lambda - Lambda @ D - D.T @ Lambda) @ X +
-                                 D.T @ Lambda @ np.maximum(0, (D @ X + E @ U + f @ np.ones(m).T)) -
-                                 Lambda @ (E @ U + f @ np.ones(m).T))
-    lip_X = (1 / m) * (np.linalg.norm(A.T @ A + Lambda - Lambda @ D + D.T @ Lambda, ord="fro") +
-                       np.max(Lambda) * (np.linalg.norm(D, ord="fro") ** 2))
-    return grad_X, lip_X
+                                 D.T @ Lambda @ np.maximum(0, (D @ X + E @ U + f @ np.ones(m).reshape(1, m))) -
+                                 Lambda @ (E @ U + f @ np.ones(m).reshape(1, m)))
+    return grad_X
 
 def Omega_ABc(theta, X, U, Y):
     A, B, c = theta["A"], theta["B"], theta["c"]
-    m = np.shape(c)
-    assert np.shape(np.outer(c, np.ones(m))) == (m, m)
-    return (1 / m) * (A @ X + B @ U + np.outer(c, np.ones(m)) - Y)
+    m = theta["m"]
+    assert np.shape(c @ np.ones(m).reshape(1, m)) == (m, m)
+    return (1 / m) * (A @ X + B @ U + c @ np.ones(m).reshape(1, m) - Y)
 
 
 def Omega_DEfLambda(m, D, E, f, Lambda, X, U):
-    return (1/m) * Lambda @ (np.maximum(0, (D @ X + E @ U + f @ np.ones(m).T)) - X)
+    return (1/m) * Lambda @ (np.maximum(0, (D @ X + E @ U + f @ np.ones(m).reshape(1, m))) - X)