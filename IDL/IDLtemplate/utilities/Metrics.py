ac2b094a9d372b15080da957afa42018b0d1df7f
Guillaume Goujard <guillaume.goujard@polytechnique.edu>
2019-11-19 19:02:42 -0800

    L2Loss

--- IDL/IDLtemplate/utilities/Metrics.py
+++ IDL/IDLtemplate/utilities/Metrics.py
@@ -5,10 +5,11 @@ def fenchtel_div(X,Y):
     Z = 0.5*(np.multiply(X, X) + np.multiply(np.maximum(0, Y), np.maximum(0, Y))) - np.multiply(X, Y)
     return np.mean(Z, axis=1)
 
+
 def loss(y, U, theta, X):
     test_satisfies_contraints(y, U, theta, X)
-    lambda_ = None
-    L2Loss(y, U, theta, X) + lambda_@fenchtel_div(X, )
+    lambda_ = np.diag(theta["Lambda"])
+    return L2Loss(y, U, theta, X) + lambda_@fenchtel_div(X, y)
 
 
 def test_satisfies_contraints(y, U, theta, X):
@@ -16,5 +17,9 @@ def test_satisfies_contraints(y, U, theta, X):
 
 
 def L2Loss(y, U, theta, X):
-    pass
+    A, B, c, D, E, f, Lambda = theta["A"], theta["B"], theta["c"], theta["D"], theta["E"], theta["f"], theta["Lambda"]
+    m = theta["m"]
+    M = A@X+ B@U + c@np.ones((1,m)) - y
+    return (1/(2*m))*np.linalg.norm(M, ord="fro")**2
+
 
